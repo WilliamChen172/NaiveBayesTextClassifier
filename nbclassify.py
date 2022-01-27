61e43cce02a9afca2d36f39d0ded14c9bc96ea99
William Chen <chenyuwilliam@gmail.com>
2022-01-27 14:45:29 -0800

    learn and classify now working properly

--- nbclassify.py
+++ nbclassify.py
@@ -1,34 +1,90 @@
+import math
+import sys
+import ast
+import os
+import glob
+
 import nblearn
 
+model_path = "nbmodel.txt"
 
-model = "nbmodel.txt"
+
+def read_input(root):
+    reviews = []
+    paths = []
+
+    pt_dir = os.path.join(root, "**/*.txt")
+    for review in glob.glob(pt_dir, recursive=True):
+        if review[-10:] == "README.txt":
+            continue
+        with open(review) as f:
+            reviews.append(f.read())
+            paths.append(review)
+    return reviews, paths
+
 
 def read_model():
-    with open(model)
+    with open(model_path) as f:
+        model = f.read()
+    model = ast.literal_eval(model)
+    return model
 
-def predict(self, reviews):
-    result = []
-    pt_prob = self.class_priors["pt"]
-    pd_prob = self.class_priors["pd"]
-    nt_prob = self.class_priors["nt"]
-    nd_prob = self.class_priors["nd"]
-    for review in reviews:
-        tokens = self.tokenize(review)
+
+def predict(model, reviews, paths):
+    results = []
+    class_priors = model["class_priors"]
+    vocabulary = model["vocabulary"]
+    word_count = model["word_count"]
+    token_count = model["token_count"]
+
+    for review, path in zip(reviews, paths):
+        tokens = nblearn.tokenize(review)
+        pt_prob = math.log(class_priors["pt"])
+        pd_prob = math.log(class_priors["pd"])
+        nt_prob = math.log(class_priors["nt"])
+        nd_prob = math.log(class_priors["nd"])
         for token in tokens:
-            if token not in self.vocabulary:
+            if token not in vocabulary:
                 continue
-
             # Add Laplace smoothing
-            pt = (self.word_count["pt"].get(token, 0) + 1) / (self.review_count["pt"] + len(self.vocabulary))
-            pd = (self.word_count["pd"].get(token, 0) + 1) / (self.review_count["pd"] + len(self.vocabulary))
-            nt = (self.word_count["nt"].get(token, 0) + 1) / (self.review_count["nt"] + len(self.vocabulary))
-            nd = (self.word_count["nd"].get(token, 0) + 1) / (self.review_count["nd"] + len(self.vocabulary))
+            pt = math.log((word_count["pt"].get(token, 0) + 1) / (token_count["pt"] + len(vocabulary)))
+            pd = math.log((word_count["pd"].get(token, 0) + 1) / (token_count["pd"] + len(vocabulary)))
+            nt = math.log((word_count["nt"].get(token, 0) + 1) / (token_count["nt"] + len(vocabulary)))
+            nd = math.log((word_count["nd"].get(token, 0) + 1) / (token_count["nd"] + len(vocabulary)))
+            pt_prob += pt
+            pd_prob += pd
+            nt_prob += nt
+            nd_prob += nd
 
-            pt_prob *= pt
-            pd_prob *= pd
-            nt_prob *= nt
-            nd_prob *= nd
+        # print(pt_prob, pd_prob, nt_prob, nd_prob)
+        # print(pt_prob, nt_prob)
+        # print(path)
+        # if pt_prob > nt_prob:
+        #     results.append("positive")
+        # elif nt_prob > pt_prob:
+        #     results.append("negative")
+        if pt_prob == max(pt_prob, pd_prob, nt_prob, nd_prob):
+            results.append("truthful positive")
+        elif pd_prob == max(pt_prob, pd_prob, nt_prob, nd_prob):
+            results.append("deceptive positive")
+        elif nt_prob == max(pt_prob, pd_prob, nt_prob, nd_prob):
+            results.append("truthful negative")
+        elif nd_prob == max(pt_prob, pd_prob, nt_prob, nd_prob):
+            results.append("deceptive negative")
 
-    return
+    return results
+
+
+def save_results(results, paths):
+    with open("nboutput.txt", "w") as f:
+        for result, path in zip(results, paths):
+            f.write(result + " " + path + "\n")
+        f.close()
+
 
 if __name__ == '__main__':
+    data_path = sys.argv[1]
+    model = read_model()
+    data, paths = read_input(data_path)
+    results = predict(model, data, paths)
+    save_results(results, paths)
--- nblearn.py
+++ nblearn.py
@@ -1,4 +1,3 @@
-import numpy as np
 import sys
 import os
 import re
@@ -13,86 +12,86 @@ fold1 = "fold1"
 fold2 = "fold2"
 fold3 = "fold3"
 fold4 = "fold4"
-# labels: 0 - pt, 1 - pd, 2 - nt, 3 - nd
 
 
-class NaiveBayes:
+def read_input(root):
+    reviews = []
+    labels = []
 
-    def read_input(self, root):
-        reviews = []
-        labels = []
+    pt_dir = os.path.join(root, p_dir, tp_dir, "**/*.txt")
+    for review in glob.glob(pt_dir):
+        with open(review) as f:
+            reviews.append(f.read())
+            labels.append("pt")
 
-        pt_dir = os.path.join(root, p_dir, tp_dir, "fold[2-4]", "*.txt")
-        for review in glob.glob(pt_dir):
-            with open(review) as f:
-                reviews.append(f.read())
-                labels.append("pt")
+    pd_dir = os.path.join(root, p_dir, d_dir, "**/*.txt")
+    for review in glob.glob(pd_dir):
+        with open(review) as f:
+            reviews.append(f.read())
+            labels.append("pd")
 
-        pd_dir = os.path.join(root, p_dir, d_dir, "fold[2-4]", "*.txt")
-        for review in glob.glob(pd_dir):
-            with open(review) as f:
-                reviews.append(f.read())
-                labels.append("pd")
+    nt_dir = os.path.join(root, n_dir, tn_dir, "**/*.txt")
+    for review in glob.glob(nt_dir):
+        with open(review) as f:
+            reviews.append(f.read())
+            labels.append("nt")
 
-        nt_dir = os.path.join(root, n_dir, tn_dir, "fold[2-4]", "*.txt")
-        for review in glob.glob(nt_dir):
-            with open(review) as f:
-                reviews.append(f.read())
-                labels.append("nt")
+    nd_dir = os.path.join(root, n_dir, d_dir, "**/*.txt")
+    for review in glob.glob(nd_dir):
+        with open(review) as f:
+            reviews.append(f.read())
+            labels.append("nd")
 
-        nd_dir = os.path.join(root, n_dir, d_dir, "fold[2-4]", "*.txt")
-        for review in glob.glob(nd_dir):
-            with open(review) as f:
-                reviews.append(f.read())
-                labels.append("nd")
+    return reviews, labels
 
-        return reviews, labels
 
-    def tokenize(self, review):
-        review = re.sub('[^a-z\s]+', ' ', review, flags=re.IGNORECASE)
-        review = re.sub('(\s+)', ' ', review)
-        review = review.lower()
-        tokens = re.split("\W+", review)
-        return tokens
+def tokenize(review):
+    review = re.sub('[^a-z\s]+', ' ', review, flags=re.IGNORECASE)
+    review = re.sub('(\s+)', ' ', review)
+    review = review.lower()
+    review = review.rstrip()
+    tokens = re.split("\W+", review)
+    return tokens
 
-    def fit(self, reviews, labels):
-        review_count = {}
-        class_priors = {}
-        vocabulary = set()
-        word_count = {"pt": {}, "pd": {}, "nt": {}, "nd": {}}
-        review_count["pt"] = sum(1 for label in labels if label == "pt")
-        review_count["pd"] = sum(1 for label in labels if label == "pd")
-        review_count["nt"] = sum(1 for label in labels if label == "nt")
-        review_count["nd"] = sum(1 for label in labels if label == "nd")
-        total_count = len(reviews)
-        class_priors["pt"] = review_count["pt"] / total_count
-        class_priors["pd"] = review_count["pd"] / total_count
-        class_priors["nt"] = review_count["nt"] / total_count
-        class_priors["nd"] = review_count["nd"] / total_count
 
-        for review, label in zip(reviews, labels):
-            tokens = self.tokenize(review)
-            for token in tokens:
-                if token not in vocabulary:
-                    vocabulary.add(token)
-                if token not in word_count[label]:
-                    word_count[label][token] = 0
-                word_count[label][token] += 1
+def fit(reviews, labels):
+    nbmodel = {}
+    review_count = {}
+    class_priors = {}
+    vocabulary = set()
+    token_count = {"pt": 0, "pd": 0, "nt": 0, "nd": 0}
+    word_count = {"pt": {}, "pd": {}, "nt": {}, "nd": {}}
+    review_count["pt"] = sum(1 for label in labels if label == "pt")
+    review_count["pd"] = sum(1 for label in labels if label == "pd")
+    review_count["nt"] = sum(1 for label in labels if label == "nt")
+    review_count["nd"] = sum(1 for label in labels if label == "nd")
+    total_count = len(reviews)
+    class_priors["pt"] = review_count["pt"] / total_count
+    class_priors["pd"] = review_count["pd"] / total_count
+    class_priors["nt"] = review_count["nt"] / total_count
+    class_priors["nd"] = review_count["nd"] / total_count
 
-        print(class_priors)
+    for review, label in zip(reviews, labels):
+        tokens = tokenize(review)
+        for token in tokens:
+            if token not in vocabulary:
+                vocabulary.add(token)
+            if token not in word_count[label]:
+                word_count[label][token] = 0
+            word_count[label][token] += 1
+            token_count[label] += 1
+    # print(word_count["pt"])
+    nbmodel["token_count"] = token_count
+    nbmodel["class_priors"] = class_priors
+    nbmodel["vocabulary"] = vocabulary
+    nbmodel["word_count"] = word_count
+    with open('nbmodel.txt', 'w') as f:
+        print(nbmodel, file=f)
 
-        with open('nbmodel.txt', 'w') as f:
-            print(review_count, file=f)
-            print(class_priors, file=f)
-            print(vocabulary, file=f)
-            print(word_count, file=f)
-
-        return
+    return
 
 
 if __name__ == '__main__':
     data_path = sys.argv[1]
-    nbmodel = NaiveBayes()
-    data, labels = nbmodel.read_input(data_path)
-    print(len(data), len(labels))
-    nbmodel.fit(data, labels)
+    data, labels = read_input(data_path)
+    fit(data, labels)
